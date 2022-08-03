02d5e74a1dd10bab41e211d84bbd33ce4ceab869
JavaClass <aleksroslik@gmail.com>
2022-08-03 10:58:34 +0200

    finished task

--- src/Week1/NumberGuesser.java
+++ src/Week1/NumberGuesser.java
@@ -8,33 +8,32 @@ public class NumberGuesser {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int numberToGuess = new Random().nextInt(100);
-        int numberOfGuesses = 5;
+        int numberOfRounds = 5;
         int guess = 0;
 
         System.out.println("Hello! What is your name?");
         String name = input.nextLine();
         System.out.println("Let's play a game " + name + " :)");
-
         System.out.println("Can you guess a number between 0 and 99? Enter your guess below:");
 
         while (guess != numberToGuess) {
-            numberOfGuesses--;
+            numberOfRounds--;
             guess = input.nextInt();
             if (guess == numberToGuess) {
                 System.out.println("You guessed it!");
                 break;
             }
-            if (guess > numberToGuess) {
+            if (guess > numberToGuess && numberOfRounds >= 1) {
                 System.out.println("Your number is GREATER than the one you are trying to guess");
-                System.out.println("Try again! You have " + numberOfGuesses + " rounds to go :)");
+                System.out.println("Try again! You have " + numberOfRounds + " round(s) to go :)");
             }
-            if (guess < numberToGuess) {
+            if (guess < numberToGuess && numberOfRounds >= 1) {
                 System.out.println("Your number is LOWER than the one you are trying to guess");
-                System.out.println("Try again! You have " + numberOfGuesses + " rounds to go :)");
+                System.out.println("Try again! You have " + numberOfRounds + " round(s) to go :)");
             }
-            if (numberOfGuesses == 0) {
+            if (numberOfRounds == 0) {
                 System.out.println("Sorry you didn't guess the number, the answer was: " + numberToGuess);
-                //input.close();
+                break;
             }
         }
     }
